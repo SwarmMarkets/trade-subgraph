d29d1b698fe5459e9c3b186f861a51f028a80540
Ilamini Dagogo <dagogodboss@gmail.com>
2022-01-06 13:50:50 +0100

    finalise two entities

--- src/mappings/dOTCManager.ts
+++ src/mappings/dOTCManager.ts
@@ -1,7 +1,8 @@
 import { CreatedOffer, CreatedOrder } from './../types/dOTC/DOTCManager';
 import { Offer, Order } from '../types/schema';
+import { BigInt } from '@graphprotocol/graph-ts';
 
-export const handleNewOffer = (event: CreatedOffer) => {
+export function handleNewOffer (event: CreatedOffer):void {
     let offer = Offer.load(event.params.offerId.toHex());
     if (offer == null) {
         offer = new Offer(event.params.offerId.toHex()); 
@@ -9,23 +10,23 @@ export const handleNewOffer = (event: CreatedOffer) => {
     offer.maker = event.params.maker;
     offer.tokenIn = event.params.tokenIn
     offer.tokenOut = event.params.tokenOut
-    offer.tokenAmountIn = event.params.amountIn
-    offer.tokenAmountOut = event.params.amountOut
-    offer.offerType = event.params.offerType
+    offer.AmountIn = event.params.amountIn
+    offer.AmountOut = event.params.amountOut
+    offer.offerType = BigInt.fromI32(event.params.offerType)
     offer.specialAddress = event.params.specialAddress
     offer.isCompleted = event.params.isComplete
     offer.save();
 }
 
-export const handleNewOrder = (event: CreatedOrder) => {
+export function handleNewOrder(event: CreatedOrder):void{
     let order = Order.load(event.params.orderId.toHex())
-    let offer = Offer.load(event.params.offerId.toHex())
+    let offer = Offer.load(event.params.offerId.toHex());
     if (order == null) {
         order = new Order(event.params.orderId.toHex());
-        order.amountPaid = event.params.amountPaid;
-        order.amountToReceive = event.params.amountToReceive;
-        order.orderedBy = event.params.orderedBy;
-        // order.offer = offer.id;
-        order.save()
     }
+    order.amountPaid = event.params.amountPaid;
+    order.amountToReceive = event.params.amountToReceive;
+    order.orderedBy = event.params.orderedBy;
+    order.offers = offer.id; 
+    order.save();
 }
