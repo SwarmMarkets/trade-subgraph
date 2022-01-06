680a866ef015aa4422856936d41dc4708757a6b9
Nikita Zasimuk <n.zasimuk@aiolane.com>
2022-01-06 21:39:57 +0300

    Normalized offer amounts

--- src/mappings/dOTCManager.ts
+++ src/mappings/dOTCManager.ts
@@ -1,32 +1,45 @@
-import { CreatedOffer, CreatedOrder } from '../types/dOTC/DOTCManager';
-import { Offer, Order } from '../types/schema';
 import { BigInt } from '@graphprotocol/graph-ts';
+import { CreatedOffer, CreatedOrder } from '../types/dOTC/DOTCManager';
+import { Offer, Order, Token } from '../types/schema';
+import { bigIntToDecimal } from './helpers';
 
-export function handleNewOffer (event: CreatedOffer):void {
-    let offer = Offer.load(event.params.offerId.toHex());
-    if (offer == null) {
-        offer = new Offer(event.params.offerId.toHex()); 
-    }
-    offer.maker = event.params.maker;
-    offer.tokenIn = event.params.tokenIn
-    offer.tokenOut = event.params.tokenOut
-    offer.amountIn = event.params.amountIn
-    offer.amountOut = event.params.amountOut
-    offer.offerType = BigInt.fromI32(event.params.offerType)
-    offer.specialAddress = event.params.specialAddress
-    offer.isCompleted = event.params.isComplete
-    offer.save();
+export function handleNewOffer(event: CreatedOffer): void {
+  let offer = Offer.load(event.params.offerId.toHex());
+  if (offer == null) {
+    offer = new Offer(event.params.offerId.toHex());
+  }
+  offer.maker = event.params.maker;
+
+  let tokenIn = Token.load(event.params.tokenIn.toHexString());
+  offer.tokenIn = tokenIn.id;
+
+  let tokenOut = Token.load(event.params.tokenOut.toHexString());
+  offer.tokenOut = tokenOut.id;
+
+  offer.amountIn = bigIntToDecimal(event.params.amountIn, tokenIn.decimals);
+  offer.amountOut = bigIntToDecimal(event.params.amountOut, tokenOut.decimals);
+
+  offer.price = offer.amountOut.div(offer.amountIn);
+
+  offer.offerType = BigInt.fromI32(event.params.offerType);
+  offer.specialAddress = event.params.specialAddress;
+  offer.isCompleted = event.params.isComplete;
+
+  offer.save();
 }
 
-export function handleNewOrder(event: CreatedOrder):void{
-    let order = Order.load(event.params.orderId.toHex())
-    let offer = Offer.load(event.params.offerId.toHex());
-    if (order == null) {
-        order = new Order(event.params.orderId.toHex());
-    }
-    order.amountPaid = event.params.amountPaid;
-    order.amountToReceive = event.params.amountToReceive;
-    order.orderedBy = event.params.orderedBy;
-    order.offers = offer.id; 
-    order.save();
+export function handleNewOrder(event: CreatedOrder): void {
+  let order = Order.load(event.params.orderId.toHex());
+  let offer = Offer.load(event.params.offerId.toHex());
+  if (order == null) {
+    order = new Order(event.params.orderId.toHex());
+  }
+  let tokenPaid = Token.load(offer.tokenOut);
+  order.amountPaid = bigIntToDecimal(event.params.amountPaid, tokenPaid.decimals);
+  let tokenToReceive = Token.load(offer.tokenIn);
+  order.amountToReceive = bigIntToDecimal(event.params.amountToReceive, tokenToReceive.decimals);
+  order.orderedBy = event.params.orderedBy;
+  order.offers = offer.id;
+
+  order.save();
 }
