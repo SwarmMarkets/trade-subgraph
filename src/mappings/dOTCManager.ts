8260d9449e583bef789c8a651eed91ec8dc7b548
Ilamini Dagogo <87694159+ilamini@users.noreply.github.com>
2022-01-12 11:02:57 +0100

    Featd otc subgraph (#16)
    * Add CompletedOfferevent, CancledOffer events
    
    * Add CompletedOfferevent, CancledOffer events fix merge conflict
    
    * Add CompletedOfferevent, CancledOffer events fix merge conflict add .env to gitignore
    
    * add .env to gitignore
    
    * Update DOTCManager address
    
    Co-authored-by: Ilamini Dagogo <dagogodboss@gmail.com>
    Co-authored-by: Nikita Zasimuk <n.zasimuk@aiolane.com>

--- src/mappings/dOTCManager.ts
+++ src/mappings/dOTCManager.ts
@@ -1,34 +1,30 @@
+import { CreatedOffer, CreatedOrder, CompletedOffer, CancledOffer } from './../types/dOTC/DOTCManager';
+import { Offer, Order } from '../types/schema';
 import { BigInt } from '@graphprotocol/graph-ts';
 import { CreatedOffer, CreatedOrder } from '../types/dOTC/DOTCManager';
 import { Offer, Order, Token } from '../types/schema';
 import { bigIntToDecimal } from './helpers';
 
-export function handleNewOffer(event: CreatedOffer): void {
-  let offer = Offer.load(event.params.offerId.toHex());
-  if (offer == null) {
-    offer = new Offer(event.params.offerId.toHex());
-  }
-  offer.maker = event.params.maker;
-
-  let tokenIn = Token.load(event.params.tokenIn.toHexString());
-  offer.tokenIn = tokenIn.id;
-
-  let tokenOut = Token.load(event.params.tokenOut.toHexString());
-  offer.tokenOut = tokenOut.id;
-
-  offer.amountIn = bigIntToDecimal(event.params.amountIn, tokenIn.decimals);
-  offer.amountOut = bigIntToDecimal(event.params.amountOut, tokenOut.decimals);
-
-  offer.price = offer.amountOut.div(offer.amountIn);
-
-  offer.offerType = BigInt.fromI32(event.params.offerType);
-  offer.specialAddress = event.params.specialAddress;
-  offer.isCompleted = event.params.isComplete;
-
-  offer.save();
+export function handleNewOffer (event: CreatedOffer):void {
+    let offer = Offer.load(event.params.offerId.toHex());
+    if (offer == null) {
+        offer = new Offer(event.params.offerId.toHex());
+    }
+    offer.maker = event.params.maker;
+    let tokenIn = Token.load(event.params.tokenIn.toHexString());
+    offer.tokenIn = tokenIn.id;
+    let tokenOut = Token.load(event.params.tokenOut.toHexString());
+    offer.tokenOut = tokenOut.id;
+    offer.amountIn = bigIntToDecimal(event.params.amountIn, tokenIn.decimals);
+    offer.amountOut = bigIntToDecimal(event.params.amountOut, tokenOut.decimals);
+    offer.price = offer.amountOut.div(offer.amountIn);
+    offer.offerType = BigInt.fromI32(event.params.offerType);
+    offer.specialAddress = event.params.specialAddress;
+    offer.isCompleted = event.params.isComplete;
+    offer.save();
 }
 
-export function handleNewOrder(event: CreatedOrder): void {
+export function handleNewOrder(event: CreatedOrder):void{
   let order = Order.load(event.params.orderId.toHex());
   let offer = Offer.load(event.params.offerId.toHex());
   if (order == null) {
@@ -43,3 +39,20 @@ export function handleNewOrder(event: CreatedOrder): void {
 
   order.save();
 }
+
+export function handleOfferCompleted(event: CompletedOffer): void{
+    let offer = Offer.load(event.params.offerId.toHex());
+    if (offer != null) {
+        offer.isCompleted = true;
+        offer.save();
+    }
+}
+
+export function handleCanceledOffer(event: CancledOffer): void{
+    let offer = Offer.load(event.params.offerId.toHex());
+    if (offer != null) {
+        offer.cancelled = true;
+        offer.save();
+    }
+}
+
