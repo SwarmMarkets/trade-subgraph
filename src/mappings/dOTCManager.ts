78d64c47ce2d124a01f68e7854ec16667cf459df
Ilamini Dagogo <87694159+ilamini@users.noreply.github.com>
2022-02-17 12:16:53 +0100

    SM-276 | feat: dOTC subgraph update to handle ERC 1155 (#63)
    * Add ERC1155 Support, add startBlock
    
    Co-authored-by: Nikita Zasimuk
    Co-authored-by: Ilamini Dagogo <dagogodboss@gmail.com>

--- src/mappings/dOTCManager.ts
+++ src/mappings/dOTCManager.ts
@@ -4,8 +4,11 @@ import {
   CompletedOffer,
   CreatedOffer,
   CreatedOrder,
+  CreatedNftOffer,
+  CreatedNftOrder,
+  CompletedNftOffer,
 } from '../types/dOTC/DOTCManager'
-import { Offer, Order } from '../types/schema'
+import { Offer, Order, NftOffer, NftOrder } from '../types/schema'
 import { bigIntToDecimal } from './helpers'
 import { Token } from '../wrappers'
 import { ZERO_BD } from '../constants/math'
@@ -89,6 +92,55 @@ export function handleCanceledOffer(event: CanceledOffer): void {
   let offer = Offer.load(event.params.offerId.toHex())
   if (offer != null) {
     offer.cancelled = true
+    offer.save()
+  }
+}
+
+export function handleNewNftOffer(event: CreatedNftOffer): void {
+  let offer = NftOffer.load(event.params.nftOfferId.toHex())
+  if (offer == null) {
+    offer = new NftOffer(event.params.nftOfferId.toHex())
+  }
+  offer.maker = event.transaction.from
+  offer.nftAddress = event.params.nftAddress
+  offer.nftIds = event.params.nftIds
+  offer.nftAmounts = event.params.nftAmounts
+  offer.expiresAt = event.params.expiresAt
+  let tokenOut = Token.safeLoad(event.params.tokenOutAddress.toHexString())
+  offer.tokenOut = tokenOut.id
+  offer.offerPrice = bigIntToDecimal(event.params.offerPrice, tokenOut.decimals)
+
+  offer.specialAddress = event.params.specialAddress
+  offer.isCompleted = false
+  offer.save()
+}
+
+export function handleNewNftOrder(event: CreatedNftOrder): void {
+  let order = NftOrder.load(event.params.orderId.toHex())
+  let offer = NftOffer.load(event.params.nftOfferId.toHex())
+  if (order == null) {
+    order = new NftOrder(event.params.orderId.toHex())
+  }
+
+  if (offer != null) {
+    let tokenPaid = Token.safeLoad(offer.tokenOut)
+    order.amountPaid = bigIntToDecimal(event.params.amount, tokenPaid.decimals)
+
+    order.offers = offer.id
+    offer.isCompleted = true
+    offer.save()
+  } else {
+    order.amountPaid = ZERO_BD
+  }
+
+  order.orderedBy = event.params.taker
+  order.save()
+}
+
+export function handleNftOfferCompleted(event: CompletedNftOffer): void {
+  let offer = Offer.load(event.params.offerId.toHex())
+  if (offer != null) {
+    offer.isCompleted = true
     offer.save()
   }
 }
