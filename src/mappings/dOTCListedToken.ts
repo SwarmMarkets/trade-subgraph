af05b783fa6118cd269c9e742ae3847397303f00
Ilamini Dagogo <dagogodboss@gmail.com>
2022-02-17 17:39:06 +0100

    Change the indexer to token address

--- src/mappings/dOTCListedToken.ts
+++ src/mappings/dOTCListedToken.ts
@@ -6,9 +6,9 @@ import { ERC20Token, ERC1155Token } from '../types/schema'
 import { Token } from './../wrappers/token'
 
 export function addNewERC20Token(event: RegisterERC20Token): void {
-  let erc20Token = ERC20Token.load(event.transaction.index.toHex())
+  let erc20Token = ERC20Token.load(event.params.token.toHexString())
   if (erc20Token == null) {
-    erc20Token = new ERC20Token(event.transaction.index.toHex())
+    erc20Token = new ERC20Token(event.params.token.toHexString())
   }
   let newToken = Token.safeLoad(event.params.token.toHexString())
   erc20Token.token = newToken.id
@@ -16,9 +16,9 @@ export function addNewERC20Token(event: RegisterERC20Token): void {
 }
 
 export function addNewERC1155Token(event: RegisterERC1155Token): void {
-  let erc1155Token = ERC1155Token.load(event.transaction.index.toHex())
+  let erc1155Token = ERC1155Token.load(event.params.token.toHexString())
   if (erc1155Token == null) {
-    erc1155Token = new ERC1155Token(event.transaction.index.toHex())
+    erc1155Token = new ERC1155Token(event.params.token.toHexString())
   }
   erc1155Token.token = event.params.token
   erc1155Token.save()
