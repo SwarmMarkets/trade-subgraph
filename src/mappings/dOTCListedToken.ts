50cdc0abd6f555a8a21e5d6bcef2ec39525ae9b7
Ilamini Dagogo <87694159+ilamini@users.noreply.github.com>
2022-02-17 17:28:03 +0100

    Update dOTCListedToken.ts

--- src/mappings/dOTCListedToken.ts
+++ src/mappings/dOTCListedToken.ts
@@ -2,7 +2,7 @@ import {
   RegisterERC20Token,
   RegisterERC1155Token,
 } from './../types/dOTCListedTokens/TokenListManager'
-import { ERC20Token, ERC1155token } from '../types/schema'
+import { ERC20Token, ERC1155Token } from '../types/schema'
 import { Token } from './../wrappers/token'
 
 export function addNewERC20Token(event: RegisterERC20Token): void {
@@ -16,9 +16,9 @@ export function addNewERC20Token(event: RegisterERC20Token): void {
 }
 
 export function addNewERC1155Token(event: RegisterERC1155Token): void {
-  let erc1155Token = ERC1155token.load(event.transaction.index.toHex())
+  let erc1155Token = ERC1155Token.load(event.transaction.index.toHex())
   if (erc1155Token == null) {
-    erc1155Token = new ERC1155token(event.transaction.index.toHex())
+    erc1155Token = new ERC1155Token(event.transaction.index.toHex())
   }
   erc1155Token.token = event.params.token
   erc1155Token.save()
