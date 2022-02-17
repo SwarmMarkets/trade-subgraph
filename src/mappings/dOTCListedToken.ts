c504eadb4cc0d8051c1e6beadf030d7fd684163e
Nikita Zasimuk <n.zasimuk@aiolane.com>
2022-02-17 21:15:18 +0300

    load erc20 if it was not registered

--- src/mappings/dOTCListedToken.ts
+++ src/mappings/dOTCListedToken.ts
@@ -1,17 +1,36 @@
+import { DEFAULT_DECIMALS } from '../constants/common'
+import { ERC20 } from '../types/dOTCListedTokens/ERC20'
 import {
   RegisterERC20Token,
   RegisterERC1155Token,
 } from './../types/dOTCListedTokens/TokenListManager'
 import { ERC20Token, ERC1155Token } from '../types/schema'
 import { Token } from './../wrappers/token'
+import { Address } from '@graphprotocol/graph-ts'
 
 export function addNewERC20Token(event: RegisterERC20Token): void {
   let erc20Token = ERC20Token.load(event.params.token.toHexString())
   if (erc20Token == null) {
     erc20Token = new ERC20Token(event.params.token.toHexString())
   }
-  let newToken = Token.safeLoad(event.params.token.toHexString())
-  erc20Token.token = newToken.id
+
+  let newToken = Token.load(event.params.token.toHexString())
+  if (newToken == null) {
+    let erc20 = ERC20.bind(Address.fromString(erc20Token.id))
+    let tokenDecimals = erc20.try_decimals()
+    let tokenName = erc20.try_name()
+    let tokenSymbol = erc20.try_symbol()
+    erc20Token.decimals = !tokenDecimals.reverted
+      ? tokenDecimals.value
+      : DEFAULT_DECIMALS
+    erc20Token.name = !tokenName.reverted ? tokenName.value : ''
+    erc20Token.symbol = !tokenSymbol.reverted ? tokenSymbol.value : ''
+  } else {
+    erc20Token.name = newToken.name
+    erc20Token.symbol = newToken.symbol
+    erc20Token.decimals = newToken.decimals
+  }
+
   erc20Token.save()
 }
 
