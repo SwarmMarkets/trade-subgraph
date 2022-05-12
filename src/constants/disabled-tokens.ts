f5bb192fea6391222aefaaa9875dba275ebbe03b
Nikita Zasimuk <n.zasimuk@aiolane.com>
2022-05-12 13:55:19 +0300

    chore: description for disabled tokens and xTokens

--- src/constants/disabled-tokens.ts
+++ src/constants/disabled-tokens.ts
@@ -1,9 +1,20 @@
 import { getValueByNetwork, ValueByNetwork } from '../utils/getValueByNetwork'
 
+/**
+ * @description Disabled tokens for all networks:
+ *
+ * | symbol  | network |                   token                    |                  xToken
+ * |:-------:|:-------:|:------------------------------------------:|:------------------------------------------:|
+ * | SPT     |mainnet  | 0x8c4167154accd56797d122d8bcaad3a9432ed4af | 0xdde53eff3632ed4820c41ffe66cd32a42aba3899 | WBTC & DAI pool
+ * |         |         |                                            | 0x71b3c05afc180e528529e0c026501e606118894c |
+ * | SOL     |matic    | 0x7dff46370e9ea5f0bad3c4e29711ad50062ea7a4 | 0x6d1ba6cf6598ff904a30b5cb2ab946513d3d59fc |
+ *
+ */
+
 let tokensByNetwork: ValueByNetwork<Array<string>> = {
   mainnet: ['0x8c4167154accd56797d122d8bcaad3a9432ed4af'],
   rinkeby: [],
-  matic: [],
+  matic: ['0x7dff46370e9ea5f0bad3c4e29711ad50062ea7a4'],
   mumbai: [],
 }
 
@@ -13,9 +24,15 @@ let xTokensByNetwork: ValueByNetwork<Array<string>> = {
     '0x71b3c05afc180e528529e0c026501e606118894c',
   ],
   rinkeby: [],
-  matic: [],
+  matic: ['0x6d1ba6cf6598ff904a30b5cb2ab946513d3d59fc'],
   mumbai: [],
 }
 
-export let disabledTokens = getValueByNetwork<Array<string>>(tokensByNetwork, [])
-export let disabledXTokens = getValueByNetwork<Array<string>>(xTokensByNetwork, [])
+export let disabledTokens = getValueByNetwork<Array<string>>(
+  tokensByNetwork,
+  [],
+)
+export let disabledXTokens = getValueByNetwork<Array<string>>(
+  xTokensByNetwork,
+  [],
+)
