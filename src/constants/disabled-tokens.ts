800b2e0a49df28499d582d3c5b026a15e135b05f
Alaa Saleh <photonite@users.noreply.github.com>
2022-05-20 20:05:30 -0300

    add disabled SOLsn tokens on polygon & Mumbai (#161)

--- src/constants/disabled-tokens.ts
+++ src/constants/disabled-tokens.ts
@@ -8,14 +8,23 @@ import { getValueByNetwork, ValueByNetwork } from '../utils/getValueByNetwork'
  * | SPT     |mainnet  | 0x8c4167154accd56797d122d8bcaad3a9432ed4af | 0xdde53eff3632ed4820c41ffe66cd32a42aba3899 | WBTC & DAI pool
  * |         |         |                                            | 0x71b3c05afc180e528529e0c026501e606118894c |
  * | SOL     |matic    | 0x7dff46370e9ea5f0bad3c4e29711ad50062ea7a4 | 0x6d1ba6cf6598ff904a30b5cb2ab946513d3d59fc |
- *
+ * |         |         |                                            |                                            |
+ * | SOLsn   |matic    | 0xe936c599401418fd5b3f2579988d8e6f3a423939 | 0x0721c00918ffc52fc00b9530ea6cfcca65dafaf0 |
+ * |         |mumbai   | 0xaba36F3331BbBc39Ae5bD3333acd64cc3b1295b7 |                                            |
+ * |         |mumbai   | 0x6744EC61948CAA662cab48Fe59CA90123C42eb28 |                                            |
  */
 
 let tokensByNetwork: ValueByNetwork<Array<string>> = {
   mainnet: ['0x8c4167154accd56797d122d8bcaad3a9432ed4af'],
   rinkeby: [],
-  matic: ['0x7dff46370e9ea5f0bad3c4e29711ad50062ea7a4'],
-  mumbai: [],
+  matic: [
+    '0x7dff46370e9ea5f0bad3c4e29711ad50062ea7a4',
+    '0xe936c599401418fd5b3f2579988d8e6f3a423939',
+  ],
+  mumbai: [
+    '0xaba36F3331BbBc39Ae5bD3333acd64cc3b1295b7',
+    '0x6744EC61948CAA662cab48Fe59CA90123C42eb28',
+  ],
 }
 
 let xTokensByNetwork: ValueByNetwork<Array<string>> = {
@@ -24,7 +33,10 @@ let xTokensByNetwork: ValueByNetwork<Array<string>> = {
     '0x71b3c05afc180e528529e0c026501e606118894c',
   ],
   rinkeby: [],
-  matic: ['0x6d1ba6cf6598ff904a30b5cb2ab946513d3d59fc'],
+  matic: [
+    '0x6d1ba6cf6598ff904a30b5cb2ab946513d3d59fc',
+    '0x0721c00918ffc52fc00b9530ea6cfcca65dafaf0',
+  ],
   mumbai: [],
 }
 
