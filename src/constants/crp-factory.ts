c781d0e973a7b943ce46ed197a091c5d29f7e89c
Nikita Zasimuk <n.zasimuk@aiolane.com>
2022-06-17 13:54:26 +0300

    feat: goerli support

--- src/constants/crp-factory.ts
+++ src/constants/crp-factory.ts
@@ -4,6 +4,7 @@ import { getValueByNetwork, ValueByNetwork } from '../utils/getValueByNetwork'
 let CrpFactoryByNetwork: ValueByNetwork<string> = {
   mainnet: '0xed52D8E202401645eDAD1c0AA21e872498ce47D0',
   rinkeby: '0xA3F9145CB0B50D907930840BB2dcfF4146df8Ab4',
+  goerli: ZERO_ADDRESS,
   matic: ZERO_ADDRESS,
   mumbai: ZERO_ADDRESS,
 }
--- src/constants/stablecoins.ts
+++ src/constants/stablecoins.ts
@@ -4,6 +4,7 @@ import { ZERO_ADDRESS } from './common'
 let USDCByNetwork: ValueByNetwork<string> = {
   mainnet: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
   rinkeby: '0x775badfdcc4478ba25d55f076b781fd66cdaf408',
+  goerli: '', // TODO: find USDC address on goerli
   matic: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
   mumbai: '0x5fd6a096a23e95692e37ec7583011863a63214aa',
 }
@@ -11,6 +12,7 @@ let USDCByNetwork: ValueByNetwork<string> = {
 let DAIByNetwork: ValueByNetwork<string> = {
   mainnet: '0x6b175474e89094c44da98b954eedeac495271d0f',
   rinkeby: '0x98e06323f0008dd8990229c3ff299353b69491c0',
+  goerli: '', // TODO: find DAI address on goerli
   matic: '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063',
   mumbai: '0x40ff8ecf26b645bddde0ea55fc92ba9f9795d2ef',
 }
